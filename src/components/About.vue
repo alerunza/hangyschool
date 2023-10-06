fb661563b95d36438d58f4f8ce11253fc7f64280
runzac <91915526+alerunza@users.noreply.github.com>
2023-10-06 15:36:46 +0200

    update about & gamedesc

--- src/components/About.vue
+++ src/components/About.vue
@@ -15,22 +15,25 @@
         <div class="d-flex align-items-center mt-4 w-75 mx-auto">
           <p class="fs-4" v-html="$t('about.description1')"></p>
         </div>
+        <!-- <div class="d-flex align-items-center mt-4 w-75 mx-auto">
+          <p class="fs-4" v-html="$t('about.description2')"></p>
+        </div> -->
       </div>
       <div class="row">
+        <div class="col-lg-4 offset-lg-2">
+          <img class="img-fluid img-thumbnail shadow-lg rounded mt-5 w-100" src="../assets/images/photo3.jpeg"
+            alt="About Us Image" />
+        </div>
         <div class="col-lg-4">
           <img class="img-fluid img-thumbnail shadow-lg rounded mt-5 w-100" src="../assets/images/photo4.jpg"
             alt="About Us Image" />
         </div>
-        <div class="col-lg-7 d-flex align-items-center">
-          <p class="fs-4" v-html="$t('about.description2')"></p>
+        <div class="col-lg-4 offset-lg-2">
+          <img class="img-fluid img-thumbnail shadow-lg rounded mt-5 w-100" src="../assets/images/photo2.jpeg"
+            alt="About Us Image" />
         </div>
-      </div>
-      <div class="row">
-        <div class="col-lg-7 d-flex align-items-center mt-5">
-          <p class="fs-4" v-html="$t('about.description3')"></p>
-        </div>
-        <div class="col-lg-4">
-          <img class="img-fluid img-thumbnail shadow-lg rounded mt-5 w-100" src="../assets/images/photo3.jpeg"
+        <div class="col-lg-4 d-flex align-items-center">
+          <img class="img-fluid img-thumbnail shadow-lg rounded mt-5 w-100" src="../assets/images/photo5.jpeg"
             alt="About Us Image" />
         </div>
       </div>
--- src/components/GameDesc.vue
+++ src/components/GameDesc.vue
@@ -8,7 +8,7 @@
         <h1 class="fw-bold text-center fs-1 mb-5">{{ $t("gameDescription.title") }}</h1>
       </div>
       <div class="row">
-        <div class="col-lg-3">
+        <div class="col-lg-3 d-flex align-items-center">
           <img class="img-thumbnail shadow-lg rounded mb-5" src="../assets/images/photo4.jpeg"
             alt="Game Description Image" />
         </div>
@@ -16,16 +16,15 @@
           <p class="fs-4 mb-5" v-html="$t('gameDescription.description1')"></p>
         </div>
       </div>
-      <div class="row my-3">
+      <!-- <div class="row my-3">
         <div class="col-lg-7 d-flex align-items-center">
           <p class="fs-4" v-html="$t('gameDescription.description2')"></p>
         </div>
         <div class="col-lg-5">
-          <img class="img-thumbnail shadow-lg rounded mt-5" src="../assets/images/photo2.jpeg"
-            alt="Game Description Image" />
+          <img class="img-thumbnail shadow-lg rounded" src="../assets/images/photo5.jpeg" alt="Game Description Image" />
         </div>
-      </div>
-      <div class="row">
+      </div> -->
+      <!-- <div class="row">
         <div class="col-lg-6">
           <img class="img-thumbnail shadow-lg rounded mt-5 mb-5" src="../assets/images/photo5.jpeg"
             alt="Game Description Image" />
@@ -33,7 +32,7 @@
         <div class="col-lg-6 d-flex align-items-center">
           <p class="fs-4" v-html="$t('gameDescription.description3')"></p>
         </div>
-      </div>
+      </div> -->
     </div>
   </section>
 </template>
